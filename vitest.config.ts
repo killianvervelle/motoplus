a8ddae712e66f17170961e8009a9e08c589a8ba6
killian vervelle <killianvervelle@hotmail.com>
2025-08-29 15:31:42 +0200

    setting up unit testing with Vitest and e2e testing with playwrite

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['**/*.tests.unit.{ts,tsx}', '**/*.spec.{ts,tsx}'], 
+    include: ['**/*.tests.unit.{ts,tsx}', '**/*.test.{ts,tsx}'], 
     exclude: [
       '**/*.e2e.*',        
       '**/node_modules/**',
