fe99765be1304a34616016622f46ef1b1f170d56
killian vervelle <killianvervelle@hotmail.com>
2025-08-31 00:32:26 +0200

    deploying pipeline on Vercel for stagging e2e  and prod

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,6 @@ const basePath =
     ? siteBasePath
     : undefined;
 
-console.log('[build] basePath =', basePath);
-
 export default {
   reactStrictMode: true,
   ...(basePath ? { basePath } : {}),
