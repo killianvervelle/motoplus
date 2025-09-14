c7402d175500ce417f9cd59747c9d95046b43149
killian vervelle <killianvervelle@hotmail.com>
2025-09-14 19:01:30 +0200

    final release with adjustements

--- src/app/[locale]/products/[slug]/page.tsx
+++ src/app/[locale]/products/[slug]/page.tsx
@@ -1,18 +1,9 @@
-import Social from "@/components/Social";
-import { AddToCart } from "@/components/cart/AddToCart";
+
 import LoadingProductGallery from "@/components/loadings/skeleton/SkeletonProductGallery";
-import ProductGallery from "@/components/product/ProductGallery";
-import ShowTags from "@/components/product/ShowTags";
-import Tabs from "@/components/product/Tabs";
-import config from "@/config/config.json";
-import { getListPage } from "@/lib/contentParser";
-import { getProduct, getProductRecommendations } from "@/lib/shopify";
-import LatestProducts from "@/partials/FeaturedProducts";
+import { getProduct,  } from "@/lib/shopify";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
-export const dynamicParams = true;
-export const dynamic = 'force-dynamic';
 
 export const generateMetadata = async ({
   params,
@@ -29,148 +20,25 @@ export const generateMetadata = async ({
   };
 };
 
-const ProductSingle = async ({
-  params
-}: {
-  params: Promise<{ locale: string; slug: string }>
-}) => {
-  const param = await params
+const ProductSingle = async () => {
   return (
     <Suspense fallback={<LoadingProductGallery />}>
-      <ShowProductSingle params={param} />
+      <ShowProductSingle />
     </Suspense>
   );
 };
 
 export default ProductSingle;
 
-const ShowProductSingle = async ({ params }: { params: { locale: string; slug: string } }) => {
-  const paymentsAndDelivery = getListPage("sections/payments-and-delivery.md");
-  const { payment_methods, estimated_delivery } =
-    paymentsAndDelivery.frontmatter;
+const ShowProductSingle = async () => {
 
-  const { currencySymbol } = config.shopify;
-  const product = await getProduct(params.slug);
 
-  if (!product) return notFound();
 
-  const {
-    id,
-    title,
-    description,
-    descriptionHtml,
-    priceRange,
-    images,
-    variants,
-    tags,
-  } = product;
-
-  const relatedProducts = await getProductRecommendations(id);
-
-  const defaultVariantId = variants.length > 0 ? variants[0].id : undefined;
 
   return (
     <>
       <section className="md:section-sm">
-        <div className="container">
-          <div className="row justify-center">
-            {/* left side contents  */}
-            <div className="col-10 md:col-8 lg:col-6">
-              <Suspense>
-                <ProductGallery images={images} />
-              </Suspense>
-            </div>
 
-            <div className="col-10 md:col-8 lg:col-5 md:ml-7 py-6 lg:py-0">
-              <h1 className="text-3xl md:h2 mb-2 md:mb-6">{title}</h1>
-
-              <div className="flex gap-2 items-center">
-                <h4 className="text-[#c60404] max-md:h2">
-                  {currencySymbol} {priceRange?.minVariantPrice.amount}{" "}
-                  {priceRange?.minVariantPrice?.currencyCode}
-                </h4>
-
-              </div>
-
-              <div className="flex gap-4 mt-8 md:mt-10 mb-6">
-                <Suspense>
-                  <AddToCart
-                    variants={product?.variants}
-                    availableForSale={product?.availableForSale}
-                    stylesClass={"btn max-md:btn-sm btn-primary hover:bg-gray-700"}
-                    handle={null}
-                    defaultVariantId={defaultVariantId}
-                  />
-                </Suspense>
-              </div>
-
-              <div className="mb-8 md:mb-10">
-                <p className="p-2 max-md:text-sm rounded-md bg-light dark:bg-darkmode-light inline">
-                  {estimated_delivery}
-                </p>
-              </div>
-
-              <div className="flex flex-wrap items-center gap-3">
-                <h5 className="max-md:text-base">Payment: </h5>
-                {payment_methods?.map(
-                  (payment: { name: string; image_url: string }) => (
-                    <img
-                      key={payment.name}
-                      src={payment.image_url}
-                      alt={payment.name}
-                      width={44}
-                      height={40}
-                      className="w-[44px] h-[40px]"
-                    />
-                  ),
-                )}
-              </div>
-
-              <hr className="my-6 border border-[#cecece] dark:border-border/40" />
-
-              <div className="flex gap-3 items-center mb-6">
-                <h5 className="max-md:text-base">Share:</h5>
-                <Social socialName={title} className="social-icons" />
-              </div>
-
-              {tags.length > 0 && (
-                <div className="flex flex-wrap gap-3 items-center">
-                  <h5 className="max-md:text-base">Tags:</h5>
-                  <Suspense>
-                    <ShowTags tags={tags} />
-                  </Suspense>
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
-      </section>
-
-      {/* Description of a product  */}
-      {description && (
-        <section>
-          <div className="container">
-            <div className="row">
-              <div className="col-10 lg:col-11 mx-auto">
-                <Tabs descriptionHtml={descriptionHtml} />
-              </div>
-            </div>
-          </div>
-        </section>
-      )}
-
-      {/* Recommended Products section  */}
-      <section className="section-bottom">
-        <div className="container">
-          {relatedProducts?.length > 0 && (
-            <>
-              <div className="text-center mb-6 md:mb-14 pt-24">
-                <h3 className="mb-2">Related Products</h3>
-              </div>
-              <LatestProducts products={relatedProducts.slice(0, 4)} />
-            </>
-          )}
-        </div>
       </section>
     </>
   );
