6c5ba99a45d1d33a1933b9ecac43b2e8545cbe62
killian vervelle <killianvervelle@hotmail.com>
2025-09-14 16:48:53 +0200

    final release with adjustements

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -12,9 +12,6 @@ import { NextIntlClientProvider, hasLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
 
-export function generateStaticParams() {
-  return routing.locales.map((locale) => ({ locale }));
-}
 
 export default async function RootLayout({ children, params }: { children: React.ReactNode, params: Promise<{ locale: string }>; }) {
   const pf = theme.fonts.font_family.primary
