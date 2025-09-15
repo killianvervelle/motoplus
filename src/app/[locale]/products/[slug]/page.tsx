c0868de0adbf12bec74336fb5227813a3b27d46a
killian vervelle <killianvervelle@hotmail.com>
2025-09-15 14:09:49 +0200

    final release with adjustements

--- src/app/[locale]/products/[slug]/page.tsx
+++ src/app/[locale]/products/[slug]/page.tsx
@@ -1,6 +1,11 @@
-
+import Social from "@/components/Social";
+import { AddToCart } from "@/components/cart/AddToCart";
 import LoadingProductGallery from "@/components/loadings/skeleton/SkeletonProductGallery";
-import { getProduct,  } from "@/lib/shopify";
+import ProductGallery from "@/components/product/ProductGallery";
+import ShowTags from "@/components/product/ShowTags";
+import config from "@/config/config.json";
+import { getListPage } from "@/lib/contentParser";
+import { getProduct } from "@/lib/shopify";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
@@ -20,26 +25,121 @@ export const generateMetadata = async ({
   };
 };
 
-const ProductSingle = async () => {
+const ProductSingle = async ({
+  params
+}: {
+  params: { locale: string; slug: string }
+}) => {
   return (
     <Suspense fallback={<LoadingProductGallery />}>
-      <ShowProductSingle />
+      <ShowProductSingle params={params} />
     </Suspense>
   );
 };
 
 export default ProductSingle;
 
-const ShowProductSingle = async () => {
+const ShowProductSingle = async ({ params }: { params: { locale: string; slug: string } }) => {
+  const paymentsAndDelivery = getListPage("sections/payments-and-delivery.md");
+  const { payment_methods, estimated_delivery } =
+    paymentsAndDelivery.frontmatter;
 
+  const { currencySymbol } = config.shopify;
+  const product = await getProduct(params.slug);
 
+  if (!product) return notFound();
 
+  const {
+    title,
+    
+    priceRange,
+    images,
+    variants,
+    tags,
+  } = product;
+
+  //const relatedProducts = await getProductRecommendations(id);
+
+  const defaultVariantId = variants.length > 0 ? variants[0].id : undefined;
 
   return (
     <>
       <section className="md:section-sm">
+        <div className="container">
+          <div className="row justify-center">
+            {/* left side contents  */}
+            <div className="col-10 md:col-8 lg:col-6">
+              <Suspense>
+                <ProductGallery images={images} />
+              </Suspense>
+            </div>
 
+            <div className="col-10 md:col-8 lg:col-5 md:ml-7 py-6 lg:py-0">
+              <h1 className="text-3xl md:h2 mb-2 md:mb-6">{title}</h1>
+
+              <div className="flex gap-2 items-center">
+                <h4 className="text-[#c60404] max-md:h2">
+                  {currencySymbol} {priceRange?.minVariantPrice.amount}{" "}
+                  {priceRange?.minVariantPrice?.currencyCode}
+                </h4>
+
+              </div>
+
+              <div className="flex gap-4 mt-8 md:mt-10 mb-6">
+                <Suspense>
+                  <AddToCart
+                    variants={product?.variants}
+                    availableForSale={product?.availableForSale}
+                    stylesClass={"btn max-md:btn-sm btn-primary hover:bg-gray-700"}
+                    handle={null}
+                    defaultVariantId={defaultVariantId}
+                  />
+                </Suspense>
+              </div>
+
+              <div className="mb-8 md:mb-10">
+                <p className="p-2 max-md:text-sm rounded-md bg-light dark:bg-darkmode-light inline">
+                  {estimated_delivery}
+                </p>
+              </div>
+
+              <div className="flex flex-wrap items-center gap-3">
+                <h5 className="max-md:text-base">Payment: </h5>
+                {payment_methods?.map(
+                  (payment: { name: string; image_url: string }) => (
+                    <img
+                      key={payment.name}
+                      src={payment.image_url}
+                      alt={payment.name}
+                      width={44}
+                      height={40}
+                      className="w-[44px] h-[40px]"
+                    />
+                  ),
+                )}
+              </div>
+
+              <hr className="my-6 border border-[#cecece] dark:border-border/40" />
+
+              <div className="flex gap-3 items-center mb-6">
+                <h5 className="max-md:text-base">Share:</h5>
+                <Social socialName={title} className="social-icons" />
+              </div>
+
+              {tags.length > 0 && (
+                <div className="flex flex-wrap gap-3 items-center">
+                  <h5 className="max-md:text-base">Tags:</h5>
+                  <Suspense>
+                    <ShowTags tags={tags} />
+                  </Suspense>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </section>
+
+
     </>
   );
 };
