87e3f1587513b0befa9c202064c2d3e5e1b647a7
killian vervelle <killianvervelle@hotmail.com>
2025-09-15 17:41:09 +0200

    final release with adjustements

--- src/app/[locale]/products/[slug]/page.tsx
+++ src/app/[locale]/products/[slug]/page.tsx
@@ -98,11 +98,7 @@ const ShowProductSingle = async ({ params }: { params: { locale: string; slug: s
                 </Suspense>
               </div>
 
-              <div className="mb-8 md:mb-10">
-                <p className="p-2 max-md:text-sm rounded-md bg-light dark:bg-darkmode-light inline">
-                  {estimated_delivery}
-                </p>
-              </div>
+
 
 
 
