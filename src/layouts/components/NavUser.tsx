7eec60a315a7ca4456f5c6961b37cf35f7ff2682
killian vervelle <killianvervelle@hotmail.com>
2025-10-02 17:44:32 +0200

    final push

--- src/layouts/components/NavUser.tsx
+++ src/layouts/components/NavUser.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import Gravatar from 'react-gravatar'
 import { BsPerson } from 'react-icons/bs'
@@ -30,7 +30,7 @@ async function fetchUser(): Promise<UserInfo | null> {
 
 const NavUser = () => {
   const pathname = usePathname()
-  const router = useRouter()
+  //const router = useRouter()
   const [user, setUser] = useState<UserInfo | null>(null)
   const [dropdownOpen, setDropdownOpen] = useState(false)
 
@@ -44,17 +44,8 @@ const NavUser = () => {
     getUser()
   }, [pathname])
 
-  const handleLogout = async () => {
-    try {
-      await fetch("/api/customer/auth/logout", { method: "POST", credentials: "include" });
-    } catch (err) {
-      console.error("Failed to logout:", err);
-    }
-
-    localStorage.removeItem("user");
-    setUser(null);
-    setDropdownOpen(false);
-    router.push("/login");
+  const handleLogout = () => {
+    window.location.href = "/api/customer/auth/logout";
   };
 
   const toggleDropdown = () => {
